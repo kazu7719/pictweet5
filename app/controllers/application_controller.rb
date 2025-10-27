665d4154c3cc79eff416a41f3839587a034e6ada
kazu7719 <kazuki.eripon7719@icloud.com>
2025-10-27 13:24:19 +0900

    ユーザー管理機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :devise_parameter, if: :devise_controller?
+     
+  private
+  def devise_parameter
+    devise_parameter_sanitizer.permit(:sign_uo, keys: [:nickname])
+  end
 end
