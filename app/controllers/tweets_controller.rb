a08804f5cbfa2e369d68974482e7d6764af1c49a
kazu7719 <kazuki.eripon7719@icloud.com>
2025-10-27 13:02:45 +0900

    ツイート編集機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,16 @@ class TweetsController < ApplicationController
 
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
