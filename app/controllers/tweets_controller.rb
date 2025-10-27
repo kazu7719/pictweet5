5f56d0a3a88e3426b8ad928c497178d81eb897e0
kazu7719 <kazuki.eripon7719@icloud.com>
2025-10-27 13:11:00 +0900

    ツイート詳細表示機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
 
   def index
     @tweets = Tweet.all
@@ -21,7 +22,7 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
+     
   end
 
   def update
@@ -30,8 +31,16 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
